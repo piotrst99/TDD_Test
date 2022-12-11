11e43f03d7d2fef77faffe3b15e7eb3f07c53d9a
piotrst99 <piotrstukus@outlook.com>
2022-12-11 18:59:24 +0100

    Add project files.

---
+++ P4_TDD_Test/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace P4_TDD_Test {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TDD_Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+
+namespace TDD_Test {
+    [TestFixture]
+    public class UnitTest1 {
+        [Test]
+        public void TestMethod1() {
+            
+        }
+    }
+}
