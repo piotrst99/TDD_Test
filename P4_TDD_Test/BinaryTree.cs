2ff2e17ace6743aac3af7f22fd2eaee9dce05f43
piotrst99 <piotrstukus@outlook.com>
2022-12-12 21:17:47 +0100

    Test

---
+++ P4_TDD_Test/BinaryTree.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_TDD_Test {
+    public class BinaryTree {
+        private Node _root { get; set; }
+
+        public BinaryTree() {
+            _root = null;
+        }
+
+        public void Insert(int value) {
+            if(_root == null) {
+                _root = new Node(value);
+                return;
+            }
+
+            InsertData(_root, value);
+        }
+
+        private void InsertData(Node parent, int value) {
+            if(parent == null) {
+                parent = new Node(value);
+            }
+            if(value > parent.Data) {
+                if(parent.RightNode == null) {
+                    parent.RightNode = new Node(value);
+                }
+                else {
+                    InsertData(parent.RightNode, value);
+                }
+            }
+            else {
+                if (parent.LeftNode == null) {
+                    parent.LeftNode = new Node(value);
+                }
+                else {
+                    InsertData(parent.LeftNode, value);
+                }
+            }
+        }
+
+        private void DisplayTree(Node root) {
+            if (root == null) return;
+
+            DisplayTree(root.LeftNode);
+            System.Console.Write(root.Data + " ");
+            DisplayTree(root.RightNode);
+        }
+
+        private void DisplayTree(Node root, string symbol, char c) {
+            if (root == null) return;
+
+            DisplayTree(root.RightNode, "\t" + symbol,'R');
+            if (c == 'O') {
+                System.Console.Write(symbol + root.Data + "\n");
+            }
+            else if (c == 'R') {
+                Console.ForegroundColor = ConsoleColor.White;
+                System.Console.Write(symbol + root.Data + "\n");
+            }
+            else {
+                Console.ForegroundColor = ConsoleColor.Green;
+                System.Console.Write(symbol + root.Data + "\n");
+            }
+            DisplayTree(root.LeftNode, "\t"+symbol,'L');
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void DisplayTree() {
+            DisplayTree(_root, "", 'O');
+        }
+
+        public void DrawBinaryTree() {
+
+        }
+
+        public bool IsNull() {
+            return _root == null;
+        }
+
+        public Node GetTree() {
+            return _root;
+        }
+    }
+}
--- P4_TDD_Test/Program.cs
+++ P4_TDD_Test/Program.cs
@@ -3,7 +3,49 @@
 namespace P4_TDD_Test {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            /*BinaryTree binaryTree = new BinaryTree();
+            Console.WriteLine(binaryTree.IsNull());
+            BinaryTree binaryTree2 = new BinaryTree(10);
+            Console.WriteLine(binaryTree2.IsNull());
+            Console.WriteLine(binaryTree2.Root.Data);
+            binaryTree.AddValue(5);
+            Console.WriteLine(binaryTree2.Root.LeftNode.Data);*/
+            //Console.WriteLine(binaryTree2.Root.RightNode.Data);
+
+            BinaryTree binaryTree = new BinaryTree();
+            /*Console.WriteLine(binaryTree.IsNull());
+            binaryTree.Insert(10);
+            Console.WriteLine(binaryTree.IsNull());
+            Console.WriteLine(binaryTree.GetTree().Data);
+            binaryTree.Insert(20);
+            Console.WriteLine(binaryTree.GetTree().RightNode.Data);
+            binaryTree.Insert(15);
+            Console.WriteLine(binaryTree.GetTree().RightNode.LeftNode.Data);*/
+            /*binaryTree.Insert(25);
+            binaryTree.Insert(30);
+            binaryTree.Insert(27);
+            binaryTree.Insert(5);
+            binaryTree.Insert(3);
+            binaryTree.Insert(4);
+            binaryTree.Insert(33);
+            binaryTree.Insert(22);
+            binaryTree.Insert(18);
+            binaryTree.Insert(12);
+            binaryTree.Insert(-1);
+            binaryTree.Insert(2);
+            binaryTree.Insert(40);
+            binaryTree.Insert(40);
+            binaryTree.Insert(19);
+            binaryTree.Insert(7);
+            binaryTree.Insert(6);
+            binaryTree.Insert(5);
+            binaryTree.Insert(17);*/
+
+            for (int i = 0; i < 25; i++) {
+                binaryTree.Insert(new Random().Next(1, 200));
+            }
+
+            binaryTree.DisplayTree();
         }
     }
 }
